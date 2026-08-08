87f2eb37d7d83f018e99c03b1ed2e7ae76a50e48
dtmmftirs-pixel <dtmm.ftirs@gmail.com>
2026-08-08 16:05:44 +0700

    Update Google Apps Script URL and Client ID

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // KONFIGURASI - GANTI DENGAN DATA ANDA
 
 // URL Web App Google Apps Script Anda
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby0WzEh0MImOWHh3-XtUsezbvAUsqsa1bwzFo5RxsSsbVGJN_AFKtE35M3sZt5efzLh/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby1HXfyGXZYNBISqZagh14flYVxVOeASeFHvzEOs-4n53-yGBoWGYRL0mauohrpOV2SvQ/exec';
 
 // Client ID Google OAuth Anda
-const GOOGLE_CLIENT_ID = '96706539653-e1h205ej3ne8uhmehl6nfe4bdhgkimm2.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = 'AIzaSyAzaCumDQBQigjhANajd5diDnBNIgy11GM.apps.googleusercontent.com';
