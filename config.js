892a8b306a2da68669b98e0eb7a9e4cba28eb8a7
dtmmftirs-pixel <dtmm.ftirs@gmail.com>
2026-08-08 16:29:02 +0700

    Update Google Client ID in config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby1HXfyGXZYNBISqZagh14flYVxVOeASeFHvzEOs-4n53-yGBoWGYRL0mauohrpOV2SvQ/exec';
 
 // Client ID Google OAuth Anda
-const GOOGLE_CLIENT_ID = 'AIzaSyAzaCumDQBQigjhANajd5diDnBNIgy11GM.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '1030656099156-tnv8ppo10n62itnt8cf9uo2a3s63bfpq.apps.googleusercontent.com';
