2974f058539205ef90ebc93d11b71e372d02dc4f
dtmmftirs-pixel <dtmm.ftirs@gmail.com>
2026-08-08 22:58:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyWwWA4TUyIaBccz84QI8rrcCRQGsptSbNF7J1gQcmCfNdcl3WkJyAbGm9Yf4psv8VfsQ/exec';
 
 // Client ID Google OAuth Anda
-const GOOGLE_CLIENT_ID = '1030656099156-tnv8ppo10n62itnt8cf9uo2a3s63bfpq.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '1030656099156-l8lprln7ji651fe12of8rs3ikdpghccl.apps.googleusercontent.com';
